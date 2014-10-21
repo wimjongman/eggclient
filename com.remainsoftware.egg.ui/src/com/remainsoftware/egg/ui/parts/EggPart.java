dee185ae87cbd456ed0e7835e2657d98d1312a6e
Wim Jongman <wim.jongman@remainsoftware.com>
2014-10-22 01:24:27 +0200

    Adding cool stuff

---
+++ com.remainsoftware.egg.ui/src/com/remainsoftware/egg/ui/parts/EggPart.java
@@ -0,0 +1,164 @@
+/*******************************************************************************
+ * Copyright (c) 2010 - 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <lars.Vogel@gmail.com> - Bug 419770
+ *******************************************************************************/
+package com.remainsoftware.egg.ui.parts;
+
+import java.util.HashMap;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.e4.ui.di.UISynchronize;
+import org.eclipse.ecf.raspberrypi.gpio.IGPIOPinOutput;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.ProgressBar;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
+
+public class EggPart implements
+		ServiceTrackerCustomizer<IGPIOPinOutput, IGPIOPinOutput> {
+
+	@Inject
+	UISynchronize fSyncer;
+
+	HashMap<ServiceReference<IGPIOPinOutput>, CTabFolder> fServiceFolder;
+
+	HashMap<String, CTabItem> fTabs = new HashMap<String, CTabItem>();
+
+	private ServiceTracker<IGPIOPinOutput, IGPIOPinOutput> fPinTracker;
+
+	private CTabFolder fTabFolder;
+
+	private CTabItem fMainTab;
+
+	@PostConstruct
+	public void createComposite(Composite pParent) {
+		System.out.println(fSyncer);
+
+		fTabFolder = new CTabFolder(pParent, SWT.CLOSE | SWT.FLAT | SWT.BOTTOM);
+		fTabFolder.setSelectionBackground(Display.getCurrent().getSystemColor(
+				SWT.COLOR_TITLE_INACTIVE_BACKGROUND_GRADIENT));
+
+		fMainTab = new CTabItem(fTabFolder, SWT.NONE);
+		fMainTab.setText("Main");
+
+		Composite composite = new Composite(fTabFolder, SWT.NONE);
+		composite.setBackground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
+		fMainTab.setControl(composite);
+		composite.setLayout(new GridLayout(1, false));
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+
+		ProgressBar progressBar = new ProgressBar(composite, SWT.INDETERMINATE);
+		progressBar.setMaximum(10);
+		progressBar.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
+				false, 1, 1));
+		new Label(composite, SWT.NONE);
+
+		Label lblScanning = new Label(composite, SWT.NONE);
+		lblScanning.setBackground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
+		lblScanning.setForeground(SWTResourceManager
+				.getColor(SWT.COLOR_DARK_GREEN));
+		lblScanning.setFont(SWTResourceManager
+				.getFont("Terminal", 13, SWT.BOLD));
+		lblScanning.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false,
+				false, 1, 1));
+		lblScanning.setText("Scanning ...");
+
+		fTabFolder.setSelection(fMainTab);
+
+		// Setup and open service tracker to get IGPIOPinOutput
+		// instances
+		initServiceTrackers();
+	}
+
+	private void initServiceTrackers() {
+		System.out.println("Init Trackers");
+		fPinTracker = new ServiceTracker<IGPIOPinOutput, IGPIOPinOutput>(
+				FrameworkUtil.getBundle(getClass()).getBundleContext(),
+				IGPIOPinOutput.class, this);
+		fPinTracker.open();
+	}
+
+	@Override
+	public IGPIOPinOutput addingService(
+			ServiceReference<IGPIOPinOutput> pReference) {
+		fSyncer.syncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Service found");
+				CTabItem tabItem = getTabItem(pReference);
+				ServiceControlComposite control = (ServiceControlComposite) tabItem
+						.getControl();
+				control.addPinService(pReference);
+				tabItem.setText(getHostName(pReference));
+				fTabFolder.setSelection(tabItem);
+			}
+
+		});
+		return FrameworkUtil.getBundle(getClass()).getBundleContext()
+				.getService(pReference);
+	}
+
+	private CTabItem getTabItem(ServiceReference<IGPIOPinOutput> pReference) {
+		CTabItem tabItem = fTabs.get(getHostName(pReference));
+		if (tabItem == null) {
+			tabItem = ServiceControlComposite.createTabItem(fTabFolder);
+			fTabs.put(getHostName(pReference), tabItem);
+		}
+		return tabItem;
+	}
+
+	private String getHostName(ServiceReference<IGPIOPinOutput> pReference) {
+		return pReference.getProperty("ecf.generic.server.hostname").toString();
+	}
+
+
+	@Override
+	public void modifiedService(ServiceReference<IGPIOPinOutput> pReference,
+			IGPIOPinOutput pService) {
+
+	}
+
+	@Override
+	public void removedService(ServiceReference<IGPIOPinOutput> pReference,
+			IGPIOPinOutput pService) {
+		fSyncer.syncExec(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Service lost");
+				CTabItem tabItem = fTabs.get(getHostName(pReference));
+				ServiceControlComposite control = (ServiceControlComposite) tabItem.getControl();
+				control.removeService(pReference);
+				if(control.getServiceCount() == 0){
+					tabItem.getControl().dispose();
+					tabItem.dispose();
+					fTabs.remove(getHostName(pReference));
+					fTabFolder.setSelection(fMainTab);
+				}
+			}
+		});
+	}
+}
---
+++ com.remainsoftware.egg.ui/src/com/remainsoftware/egg/ui/parts/ServiceControlComposite.java
@@ -0,0 +1,254 @@
+package com.remainsoftware.egg.ui.parts;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.eclipse.ecf.raspberrypi.gpio.IGPIOPin;
+import org.eclipse.ecf.raspberrypi.gpio.IGPIOPinOutput;
+import org.eclipse.ecf.raspberrypi.gpio.ILM35;
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+
+import com.remainsoftware.egg.core.OSGiUtil;
+
+public class ServiceControlComposite extends Composite implements
+		SelectionListener {
+	private TableViewer fTableViewer;
+	private HashMap<Integer, Button> fBtnPins = new HashMap<>();
+	private HashMap<Integer, ServiceReference<IGPIOPinOutput>> fServiceList = new HashMap<>();
+	private Label fLblTemp;
+	private double fTemp;
+	private ServiceRegistration<?> fTempService;
+
+	/**
+	 * Create the composite.
+	 * 
+	 * @param parent
+	 * @param style
+	 */
+	public ServiceControlComposite(Composite parent, int style) {
+		super(parent, style);
+
+		setLayout(new GridLayout(2, false));
+
+		Label lblNewLabel_1 = new Label(this, SWT.NONE);
+		lblNewLabel_1.setText("Temperature:");
+		fLblTemp = new Label(this, SWT.NONE);
+		fLblTemp.setForeground(SWTResourceManager.getColor(SWT.COLOR_GREEN));
+		fLblTemp.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.BOLD));
+		fLblTemp.setBackground(SWTResourceManager
+				.getColor(SWT.COLOR_WIDGET_DARK_SHADOW));
+		fLblTemp.setText(" 00.0C ");
+
+		initPins(parent);
+
+		Composite composite = new Composite(this, SWT.NONE);
+		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2,
+				1));
+		composite.setBounds(0, 0, 64, 64);
+		TableColumnLayout tcl_composite = new TableColumnLayout();
+		composite.setLayout(tcl_composite);
+
+		fTableViewer = new TableViewer(composite, SWT.BORDER
+				| SWT.FULL_SELECTION);
+		Table table = fTableViewer.getTable();
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+
+		TableViewerColumn tableViewerColumn = new TableViewerColumn(
+				fTableViewer, SWT.NONE);
+		TableColumn tblclmnProperty = tableViewerColumn.getColumn();
+		tcl_composite.setColumnData(tblclmnProperty, new ColumnWeightData(1,
+				ColumnWeightData.MINIMUM_WIDTH, true));
+		tblclmnProperty.setText("property");
+
+		TableViewerColumn tableViewerColumn_1 = new TableViewerColumn(
+				fTableViewer, SWT.NONE);
+		TableColumn tblclmnValue = tableViewerColumn_1.getColumn();
+		tcl_composite.setColumnData(tblclmnValue, new ColumnWeightData(1,
+				ColumnWeightData.MINIMUM_WIDTH, true));
+		tblclmnValue.setText("value");
+		fTableViewer.setContentProvider(new ArrayContentProvider());
+		fTableViewer.setLabelProvider(new PropertyLabelProvider());
+	}
+
+	private void initPins(Composite parent) {
+		addPin(parent, 0);
+		addPin(parent, 1);
+		activatePins();
+	}
+
+	private void activatePins() {
+		fBtnPins.values().forEach(btnPin -> {
+			btnPin.setEnabled(isPinServiceAvailable(btnPin));
+		});
+	}
+
+	private boolean isPinServiceAvailable(Button pBtnPin) {
+		Integer pin = (Integer) pBtnPin.getData("pin");
+		return fServiceList.get(pin) != null;
+	}
+
+	private void addPin(Composite parent, int i) {
+		new Label(this, SWT.NONE).setText("Switch Pin " + i);
+		Button btnPin = new Button(this, SWT.CHECK);
+		btnPin.setData("pin", i);
+		btnPin.addSelectionListener(this);
+		fBtnPins.put(i, btnPin);
+	}
+
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
+
+	private class PropertyLabelProvider extends LabelProvider implements
+			ITableLabelProvider {
+		public PropertyLabelProvider() {
+			super();
+		}
+
+		public Image getColumnImage(Object element, int columnIndex) {
+			return null;
+		}
+
+		public String getColumnText(Object element, int columnIndex) {
+			switch (columnIndex) {
+			case 0:
+				return element.toString();
+			case 1:
+				ServiceReference<IGPIOPinOutput> serviceRef = fServiceList
+						.values().iterator().next();
+				return propertyToString(serviceRef.getProperty(element
+						.toString()));
+			}
+			return "??";
+		}
+
+		private String propertyToString(Object pValue) {
+			if (pValue instanceof Object[]) {
+				return Arrays.deepToString((Object[]) pValue);
+			}
+			return pValue.toString();
+		}
+	}
+
+	@Override
+	public void widgetSelected(SelectionEvent e) {
+		Integer pin = (Integer) e.widget.getData("pin");
+		OSGiUtil.getService(fServiceList.get(pin), this).toggle();
+	}
+
+	@Override
+	public void widgetDefaultSelected(SelectionEvent e) {
+	}
+
+	/**
+	 * Sets the service and activates the UI. Must therefore be called in the UI
+	 * thread.
+	 * 
+	 * @param pServiceRef
+	 */
+	public void addPinService(ServiceReference<IGPIOPinOutput> pServiceRef) {
+		Integer pin = getPinFromServiceRef(pServiceRef);
+		if (pin == null) {
+			System.out.println("Invalid pin null");
+			return;
+		}
+		fServiceList.put(pin, pServiceRef);
+		activateUI(pServiceRef);
+		registerTemperatureService();
+	}
+
+	private Integer getPinFromServiceRef(
+			ServiceReference<IGPIOPinOutput> pServiceRef) {
+		Integer pin = Integer.valueOf((String) pServiceRef
+				.getProperty(IGPIOPin.PIN_ID_PROP));
+		return pin;
+	}
+
+	private void registerTemperatureService() {
+
+		if (fTempService != null) {
+			return;
+		}
+
+		System.out.println("Registering temperature service");
+
+		fTempService = OSGiUtil.RegisterService(ILM35.class, new ILM35() {
+			@Override
+			public void setTemperature(String pHost, double pTemperature) {
+				System.out.println("Temperature " + pTemperature);
+				fTemp = pTemperature;
+				getDisplay().syncExec(new Runnable() {
+					@Override
+					public void run() {
+						fLblTemp.setText(prettyTemp(fTemp));
+					}
+				});
+
+			}
+		}, this);
+
+	}
+
+	private String prettyTemp(double pTemperature) {
+		String ds = String.valueOf(pTemperature);
+		return " " + ds.substring(0, ds.indexOf(".") + 2) + "C ";
+	}
+
+	protected void activateUI(ServiceReference<IGPIOPinOutput> pServiceRef) {
+		activatePins();
+		fTableViewer.setInput(pServiceRef.getPropertyKeys());
+	}
+
+	/**
+	 * Factory method to get a Composite with a tab.
+	 * 
+	 * @param pTabFolder
+	 * @return
+	 */
+	public static CTabItem createTabItem(CTabFolder pTabFolder) {
+		CTabItem tabItem = new CTabItem(pTabFolder, SWT.NONE);
+		tabItem.setControl(new ServiceControlComposite(pTabFolder, SWT.NONE));
+		return tabItem;
+	}
+
+	@Override
+	public void dispose() {
+		System.out.println("Disposing tab.");
+		fTempService.unregister();
+		fTempService = null;
+		super.dispose();
+	}
+
+	public void removeService(ServiceReference<IGPIOPinOutput> pReference) {
+		fServiceList.remove(getPinFromServiceRef(pReference));
+		activatePins();
+	}
+
+	public int getServiceCount() {
+		return fServiceList.size();
+	}
+}
